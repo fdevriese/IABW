dbf361f5818b6335891d0e79433a2f5ef70bc3ef
fhyren <florent.devriese@gmail.com>
2014-01-16 15:57:13 +0100

    [FDE] Omagad, il scout ! Enfin il va juste dans la base enemie au bout d un temps donne et ne fait rien d autre pr le moment mais je suis content quand meme

---
+++ ExampleAIModule/Source/ExampleAIModule.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <BWAPI.h>
+
+#include <BWTA.h>
+#include <windows.h>
+
+extern bool analyzed;
+extern bool analysis_just_finished;
+extern BWTA::Region* home;
+extern BWTA::Region* enemy_base;
+DWORD WINAPI AnalyzeThread();
+
+class ExampleAIModule : public BWAPI::AIModule
+{
+public:
+  virtual void onStart();
+  virtual void onEnd(bool isWinner);
+  virtual void onFrame();
+  virtual int scout();
+  virtual void onUnitDiscover(BWAPI::Unit* unit);
+  virtual void onUnitEvade(BWAPI::Unit* unit);
+  virtual void onUnitShow(BWAPI::Unit* unit);
+  virtual void onUnitHide(BWAPI::Unit* unit);
+  virtual void onUnitCreate(BWAPI::Unit* unit);
+  virtual void onUnitDestroy(BWAPI::Unit* unit);
+  virtual void onUnitMorph(BWAPI::Unit* unit);
+  virtual void onUnitRenegade(BWAPI::Unit* unit);
+  virtual void onUnitComplete(BWAPI::Unit *unit);
+  void drawStats(); //not part of BWAPI::AIModule
+  void drawBullets();
+  void drawVisibilityData();
+  void drawTerrainData();
+  void showPlayers();
+  void showForces();
+  bool show_bullets;
+  bool show_visibility_data;
+};
